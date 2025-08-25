62212d887707aa65c723e2d2b179dc8b5c38cd7d
jsh088 <jsh088@gmail.com>
2025-08-24 17:04:59 -0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+//  import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(MyApp());
