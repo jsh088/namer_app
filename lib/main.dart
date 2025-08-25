1a9992729ae97857e9803d68367494bcea6800ac
jsh088 <jsh088@gmail.com>
2025-08-24 18:22:48 -0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:drift/drift.dart';
 
 final counterProvider = StateProvider<int>((ref) => 0);
 
